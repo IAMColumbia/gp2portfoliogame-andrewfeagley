585535c4cbe728709ac33868d4630e8c26053305
Andrew Feagley <andrew.feagley@loop.colum.edu>
2020-04-06 14:14:46 -0500

    Imports Project from wrong Repo

---
+++ assets/Assets/Scripts/DragandDrop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
+public class DragandDrop : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public TileBase swappedTile;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //swappedTile = SelectedItem.instance.tileBase;
+        if(Input.GetMouseButtonDown(0))
+        {
+            ChangeTile();
+        }
+    }
+
+    void ChangeTile()
+    {
+        Vector3 pos = Input.mousePosition;
+        Vector3Int tilePos = tilemap.WorldToCell(Camera.main.ScreenToWorldPoint(pos));
+
+        //make sure there isn't a tile where you clicked already
+        if(!tilemap.HasTile(tilePos))
+        {
+            tilemap.SetTile(tilePos, SelectedItem.Instance.tileBase);
+            Instantiate(SelectedItem.Instance.item, tilePos, Quaternion.identity);
+        }
+    }
+}
---
+++ assets/Assets/Scripts/SetItemByClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Tilemaps;
+
+public class SetItemByClick : MonoBehaviour
+{
+    [SerializeField]
+    TileBase tileBase;
+    [SerializeField]
+    GameObject objectToPlace;
+    [SerializeField]
+    Sprite objectImage;
+
+    Button button;
+
+    private void Awake()
+    {
+        button = this.gameObject.GetComponent<Button>();
+        this.button.image.sprite = objectImage;
+    }
+
+    public void SetTile()
+    {
+        SelectedItem.Instance.item = objectToPlace;
+        SelectedItem.Instance.tileBase = tileBase;
+    }
+}
